9bfdd412b76fbc91e94e25cd98d788666c7ca06f
Nasibakhon7039 <73817009+Nasibakhon7039@users.noreply.github.com>
2021-06-25 15:53:24 +0500

    Add files via upload

---
+++ WAD_WebApplication_7039/WAD_WebApplication_7039/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WAD_WebApplication_7039.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
