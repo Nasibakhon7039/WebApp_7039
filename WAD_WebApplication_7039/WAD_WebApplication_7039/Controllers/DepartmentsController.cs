7052b77f8a237677185b33d1b173514b3c2fc16c
Nasibakhon7039 <73817009+Nasibakhon7039@users.noreply.github.com>
2021-06-25 15:54:36 +0500

    Add files via upload

---
+++ WAD_WebApplication_7039/WAD_WebApplication_7039/Controllers/DepartmentsController.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Udevs.DAL.Repositories;
+using WAD_WebApplication_7039.DAL;
+using WAD_WebApplication_7039.DAL.DBO;
+using WAD_WebApplication_7039.Models;
+
+namespace WAD_WebApplication_7039.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        private readonly IRepository<Department> _departmentRepo;
+
+        public DepartmentsController(IRepository<Department> departmentRepo)
+        {
+            _departmentRepo = departmentRepo;
+        }
+
+        // GET: Departments
+        public async Task<IActionResult> Index()
+        {
+            return View(await _departmentRepo.GetAllAsync());
+        }
+
+        // GET: Departments/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var department = await _departmentRepo.GetByIdAsync(id.Value);
+            if (department == null)
+            {
+                return NotFound();
+            }
+
+            return View(department);
+        }
+
+        // GET: Departments/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Departments/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name")] Department department)
+        {
+            if (ModelState.IsValid)
+            {
+                await _departmentRepo.CreateAsync(department);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(department);
+        }
+
+        // GET: Departments/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var department = await _departmentRepo.GetByIdAsync(id.Value);
+            if (department == null)
+            {
+                return NotFound();
+            }
+            return View(department);
+        }
+
+        // POST: Departments/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name")] Department department)
+        {
+            if (id != department.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await _departmentRepo.UpdateAsync(department);
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!_departmentRepo.Exists(department.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(department);
+        }
+
+        // GET: Departments/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var department = await _departmentRepo.GetByIdAsync(id.Value);
+            if (department == null)
+            {
+                return NotFound();
+            }
+
+            return View(department);
+        }
+
+        // POST: Departments/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            await _departmentRepo.DeleteAsync(id);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ WAD_WebApplication_7039/WAD_WebApplication_7039/Controllers/EmployeesController.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Udevs.DAL.Repositories;
+using WAD_WebApplication_7039.DAL;
+using WAD_WebApplication_7039.DAL.DBO;
+using WAD_WebApplication_7039.Models;
+
+namespace WAD_WebApplication_7039.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private readonly IRepository<Employee> _employeeRepo;
+        private readonly IRepository<Department> _departmentRepo;
+
+        public EmployeesController(IRepository<Employee> employeeRepo, IRepository<Department> departmentRepo)
+        {
+            _employeeRepo = employeeRepo;
+            _departmentRepo = departmentRepo;
+        }
+
+        // GET: Employees
+        public async Task<IActionResult> Index()
+        {
+            return View(await _employeeRepo.GetAllAsync());
+        }
+
+        // GET: Employees/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var employee = await _employeeRepo.GetByIdAsync(id.Value);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            return View(employee);
+        }
+
+        // GET: Employees/Create
+        public async Task<IActionResult> Create()
+        {
+            ViewData["DepartmentId"] = new SelectList(await _departmentRepo.GetAllAsync(), "Id", "Name");
+            return View();
+        }
+
+        // POST: Employees/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,FirstName,LastName,DateOfBirth,DepartmentId,JobPosition,Salary")] Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                await _employeeRepo.CreateAsync(employee);
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["DepartmentId"] = new SelectList(await _departmentRepo.GetAllAsync(), "Id", "Name", employee.DepartmentId);
+            return View(employee);
+        }
+
+        // GET: Employees/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var employee = await _employeeRepo.GetByIdAsync(id.Value);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            ViewData["DepartmentId"] = new SelectList(await _departmentRepo.GetAllAsync(), "Id", "Name", employee.DepartmentId);
+            return View(employee);
+        }
+
+        // POST: Employees/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,FirstName,LastName,DateOfBirth,DepartmentId,JobPosition,Salary")] Employee employee)
+        {
+            if (id != employee.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await _employeeRepo.UpdateAsync(employee);
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!_employeeRepo.Exists(employee.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["DepartmentId"] = new SelectList(await _departmentRepo.GetAllAsync(), "Id", "Name", employee.DepartmentId);
+            return View(employee);
+        }
+
+        // GET: Employees/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var employee = await _employeeRepo.GetByIdAsync(id.Value);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            return View(employee);
+        }
+
+        // POST: Employees/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            await _employeeRepo.DeleteAsync(id);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
